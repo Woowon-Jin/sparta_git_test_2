8d1f6362bdd2711a9449e9fb9dd569d7b3071a13
Jin <wlsdndnjs12@naver.com>
2025-04-21 01:15:58 +0900

    feat : 내용 변경
    - 내용 변경

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Sparta
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! 12");
         }
     }
 }
